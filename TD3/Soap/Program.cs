7d365fc76615a2111f956ad44a3763323ebc7d6a
Hiba <hiba.khayati@gmail.com>
2022-03-11 10:25:49 +0100

    TD3 REST + TD4 SOAP

---
+++ TD3/Soap/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Soap.ServiceReference1;
+
+namespace Soap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorSoapClient client = new CalculatorSoapClient();
+            Console.WriteLine(client.Add(2,5));
+            Console.ReadLine();
+
+        }
+    }
+}
